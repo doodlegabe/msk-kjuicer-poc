b47f0fc9427d8fb5ad8c513c8eb806a9a569edfc
Gabriel Walsh <gabriel.walsh@moleskine.com>
2019-10-17 06:54:48 -0400

    webview added

---
+++ msk-kjuicer-poc/EditorViewController.swift
@@ -0,0 +1,29 @@
+//
+//  EditorViewController.swift
+//  msk-kjuicer-poc
+//
+//  Created by Gabriel Walsh on 10/17/19.
+//  Copyright © 2019 Gabriel Walsh. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class EditorViewController: UIViewController, WKNavigationDelegate {
+    
+    var editorWebView: WKWebView!
+    
+    override func loadView() {
+        editorWebView = WKWebView()
+        editorWebView.navigationDelegate = self
+        view = editorWebView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let url = URL(string: "https://beta.kjuicer.com/papers/i/486069b9-b244-4a2d-9a86-e81b986e5071/edit/27a8076d-518f-46e9-978c-660513a9ba34/")!
+        editorWebView.load(URLRequest(url: url))
+        editorWebView.allowsBackForwardNavigationGestures = false
+    }
+    
+}
