2a3459e0fa9640351adc39c655e22a77260a96a6
Gabriel Walsh <gabriel.walsh@moleskine.com>
2019-10-17 10:33:22 -0400

    extension stubbed out

---
+++ msk-kjuicer-poc-share/ShareViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ShareViewController.swift
+//  msk-kjuicer-poc-share
+//
+//  Created by Gabriel Walsh on 10/17/19.
+//  Copyright © 2019 Gabriel Walsh. All rights reserved.
+//
+
+import UIKit
+import Social
+import MobileCoreServices
+
+class ShareViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
